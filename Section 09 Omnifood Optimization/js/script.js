615b6b5825dbfce2e9f19e4d1de1e1dac460a3dd
Fauzan Al Ghifary <m.fauzanghifary@gmail.com>
2022-03-26 09:35:51 +0700

    FINISH THE BOOTCAMP

--- Section 09 Omnifood Optimization/js/script.js
+++ Section 09 Omnifood Optimization/js/script.js
@@ -1,23 +1,92 @@
-console.log("Hello");
+// SET CURRENT YEAR
 
-///////////////////////////////////////////////////////////
+const yearEl = document.querySelector('.year');
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
+btnNavEl.addEventListener('click', function () {
+  headerEl.classList.toggle('nav-open');
+});
+
+
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll('a:link');
+console.log(allLinks);
+
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+
+    //Scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    }
+
+    //scroll to other links
+    if (href !== "#" && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    //close mobile nav
+
+    if (link.classList.contains("main-nav-link")) {
+      if (headerEl.classList.contains("nav-open")) {
+        headerEl.classList.remove("nav-open");
+      }
+    }
+  });
+});
+
+
+// Sticky Navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(function (entries) {
+  const ent = entries[0];
+  if (ent.isIntersecting === false) {
+    document.body.classList.add('sticky');
+  }
+  if (ent.isIntersecting === true) {
+    document.body.classList.remove('sticky');
+  }
+
+},
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px"
+  });
+obs.observe(sectionHeroEl);
+
 // Fixing flexbox gap property missing in some Safari versions
-function checkFlexGap() {
-    var flex = document.createElement("div");
-    flex.style.display = "flex";
-    flex.style.flexDirection = "column";
-    flex.style.rowGap = "1px";
+// function checkFlexGap() {
+//   var flex = document.createElement("div");
+//   flex.style.display = "flex";
+//   flex.style.flexDirection = "column";
+//   flex.style.rowGap = "1px";
 
-    flex.appendChild(document.createElement("div"));
-    flex.appendChild(document.createElement("div"));
+//   flex.appendChild(document.createElement("div"));
+//   flex.appendChild(document.createElement("div"));
 
-    document.body.appendChild(flex);
-    var isSupported = flex.scrollHeight === 1;
-    flex.parentNode.removeChild(flex);
-    console.log(isSupported);
+//   document.body.appendChild(flex);
+//   var isSupported = flex.scrollHeight === 1;
+//   flex.parentNode.removeChild(flex);
+//   console.log(isSupported);
 
-    if (!isSupported) document.body.classList.add("no-flexbox-gap");
-}
-checkFlexGap();
+//   if (!isSupported) document.body.classList.add("no-flexbox-gap");
+// }
+// checkFlexGap();
 
-  // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
